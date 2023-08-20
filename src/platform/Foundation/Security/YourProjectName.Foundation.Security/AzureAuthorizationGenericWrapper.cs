b31b77d4601a062be512aa241c6103bef0a2ac10
AndiappanAR <andiappan.ravi@omnia.ae>
2023-08-21 00:45:39 +0400

    Platform code submitted

---
+++ src/platform/Foundation/Security/YourProjectName.Foundation.Security/AzureAuthorizationGenericWrapper.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Web;
+
+namespace YourProjectName.Foundation.Security
+{
+    public class AzureAuthorizationGenericProps
+    {
+        public string _audience { get; set; }
+        public string _clientId { get; set; }
+        public string _tenant { get; set; }
+        public string _authority { get; set; }
+        public bool _azureAuthorizationEnabled { get; set; }
+        public bool _validateAudience { get; set; }     
+
+    }
+
+    public class AzureAuthorizationGenericWrapper
+    {
+        private string _audience { get; set; }
+        private string _clientId { get; set; }
+        private string _tenant { get; set; }
+        private string _authority { get; set; }
+        private bool _azureAuthorizationEnabled { get; set; }
+        private bool _validateAudience { get; set; }        
+        private ConfigurationManager<OpenIdConnectConfiguration> _configManager;
+
+        public AzureAuthorizationGenericWrapper()
+        {
+            string keyPrefix = string.Concat("YourProjectName.Foundation.Security.");
+
+            _audience = Sitecore.Configuration.Settings.GetSetting(keyPrefix + "AzureAudience");
+            _clientId = Sitecore.Configuration.Settings.GetSetting(keyPrefix + "AzureClientId");
+            _tenant = Sitecore.Configuration.Settings.GetSetting(keyPrefix + "AzureTenantId");
+            _authority = Sitecore.Configuration.Settings.GetSetting(keyPrefix + "AzureAuthority");
+            _azureAuthorizationEnabled = bool.Parse(Sitecore.Configuration.Settings.GetSetting(keyPrefix + "AzureAuthorizationEnabled", "true"));
+            _validateAudience = bool.Parse(Sitecore.Configuration.Settings.GetSetting(keyPrefix + "ValidateAudience", "true"));           
+        }
+
+        public bool VerifyJwtToken(string JwtToken)
+        {
+            var request = HttpContext.Current.Request;  
+            
+            if(!_azureAuthorizationEnabled)
+            {
+                return true;
+            }
+
+            if (string.IsNullOrWhiteSpace(JwtToken))
+            {
+                return false;
+            }
+
+            if (JwtToken.StartsWith("Bearer "))
+            {
+                JwtToken = JwtToken.Replace("Bearer ", "");
+            }
+
+            Microsoft.IdentityModel.Logging.IdentityModelEventSource.ShowPII = true;
+
+            _configManager = new ConfigurationManager<OpenIdConnectConfiguration>($"{_authority}/.well-known/openid-configuration", new OpenIdConnectConfigurationRetriever());
+
+            OpenIdConnectConfiguration config = null;
+            IList<string> validissuers = new List<string>()
+                                                    {
+                                                        $"https://login.microsoftonline.com/{_tenant}/",
+                                                        $"https://login.microsoftonline.com/{_tenant}/v2.0",
+                                                        $"https://login.windows.net/{_tenant}/",
+                                                        $"https://login.microsoft.com/{_tenant}/",
+                                                        $"https://sts.windows.net/{_tenant}/"
+                                                    };
+
+            ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+
+            config = _configManager.GetConfigurationAsync(CancellationToken.None).Result;
+
+            TokenValidationParameters validationParameters = GetTokenValidationParameters(config, validissuers);
+            JsonWebTokenHandler tokenHandler = new JsonWebTokenHandler();
+            TokenValidationResult result = tokenHandler.ValidateToken(JwtToken, validationParameters);
+
+            if (result.Exception != null && result.Exception is SecurityTokenSignatureKeyNotFoundException)
+            {
+                _configManager.RequestRefresh();
+                config = _configManager.GetConfigurationAsync().Result;
+                validationParameters = GetTokenValidationParameters(config, validissuers);
+
+                // attempt to validate token again after refresh
+                result = tokenHandler.ValidateToken(JwtToken, validationParameters);
+            }
+
+            return (result.Exception == null);
+        }
+
+        private TokenValidationParameters GetTokenValidationParameters(OpenIdConnectConfiguration config, IList<string> validissuers)
+        {
+            TokenValidationParameters validationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = _validateAudience,
+                ValidateIssuer = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidAudiences = new[] { _clientId },
+                ValidIssuers = validissuers,
+                IssuerSigningKeys = config.SigningKeys
+            };
+
+            validationParameters.ValidAudiences = validationParameters.ValidAudiences.Concat(_audience.Split(',').ToList());
+
+            return validationParameters;
+        }
+
+    }
+}
+
---
+++ src/platform/Foundation/Security/YourProjectName.Foundation.Security/Pipelines/AuthorizeContextItemResolver.cs
@@ -0,0 +1,110 @@
+﻿using Sitecore;
+using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.LayoutService.Mvc.ItemResolving;
+using Sitecore.LayoutService.Mvc.Pipelines.RequestBegin;
+using Sitecore.LayoutService.Mvc.Routing;
+using Sitecore.Mvc.Pipelines.Request.RequestBegin;
+using System;
+using System.Web;
+using YourProjectName.Foundation.Security;
+
+namespace YourProjectName.Foundation.SitecoreExtension.Pipelines
+{
+    public class AuthorizeContextItemResolver : ContextItemResolver
+    {
+        public AuthorizeContextItemResolver(IItemResolver itemResolver, IRouteMapper routeMapper) : base(itemResolver, routeMapper)
+        {
+        }
+
+        public override void Process(RequestBeginArgs args)
+        {
+            base.Process(args);
+
+            if (Context.Site.IsBackend)
+            {
+                return;
+            }
+
+            if (!Settings.AliasesActive)
+            {
+                return;
+            }
+
+            var database = Context.Database;
+            if (database == null)
+            {
+                return;
+            }
+
+            if (Context.Item != null)
+            {
+                
+                string jwtToken = GetHttpHeaderValue("Authorization");
+                bool isAuthenticated = IsValidToken(jwtToken);
+
+               
+                if (!isAuthenticated)
+                {
+                    Context.Item = Context.Database.GetItem(string.Concat(Context.Site.StartPath, "/UnAuthorizedPage"));
+                    args.RequestContext.HttpContext.Response.StatusCode = 401;                   
+                    return;
+                }
+
+                string path = this.GetPath(args);
+                if (string.IsNullOrEmpty(path))
+                {
+                    return;
+                }
+                ID targetID = database.Aliases.GetTargetID(path);
+                Item item = database.GetItem(targetID);
+                Context.Item = item;               
+            }
+        }
+
+        /// <summary>
+        /// GetAuthTokenFromHeader
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        private static string GetHttpHeaderValue(string key)
+        {
+            string result = string.Empty;
+            try
+            {
+                result = HttpContext.Current.Request.Headers[key];
+            }
+            catch (Exception ex) {
+                var reflectType = System.Reflection.MethodBase.GetCurrentMethod();
+                Log.Error("ERROR:" + reflectType.ReflectedType.Name + reflectType.Name, ex, reflectType);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// IsValidToken
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        private static bool IsValidToken(string jwtToken)
+        {
+            bool isValidToken = false;
+
+            try
+            {
+                AzureAuthorizationGenericWrapper azureAuthorizationWrapper = new AzureAuthorizationGenericWrapper();
+                isValidToken = (!string.IsNullOrEmpty(jwtToken)) ? azureAuthorizationWrapper.VerifyJwtToken(jwtToken) : false;
+            }
+            catch (Exception ex)
+            {
+                var reflectType = System.Reflection.MethodBase.GetCurrentMethod();
+                Log.Error("ERROR:" + reflectType.ReflectedType.Name + reflectType.Name, ex, reflectType);
+            }
+
+            return isValidToken;
+        }
+
+    }
+}
